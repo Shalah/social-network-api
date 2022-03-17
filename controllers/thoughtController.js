e9721acee421c545854cdd9ff139280d26218445
Shalah <zoungranaala@gmail.com>
2022-03-16 20:41:26 -0400

    Working on the thoughtController. Adding some routes to it

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,10 +1,101 @@
 const { Thought, User } = require('../models');
 
-
 module.exports = {
-    // This get all the thoughts
-    
-  
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then(async (thought) => {
+        const thoughtObj = {
+          thought,
+          reactionCount: await reactionCount(),
+        };
+        return res.json(thoughtObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Get a single thought
+  getThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .then(async (thought) =>
+        !thought  // ternary operator
+          ? res.status(404).json({ message: 'No thought with that ID' })  // This is the if
+          : res.json({                                                   // This is the else
+              thought,
+              grade: await grade(req.params.thoughtId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new thought
+  createthought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a thought and remove them from the user
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No such thought exists' })
+          : User.findOneAndUpdate(
+              { thought: req.params.thoughtId },
+              { $pull: { thought: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: 'Thought deleted, but no users found',
+            })
+          : res.json({ message: 'Thought successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
 
-    
-}
+  // Add an assignment to a thought
+  addAssignment(req, res) {
+    console.log('You are adding an assignment');
+    console.log(req.body);
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { assignments: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res
+              .status(404)
+              .json({ message: 'No thought found with that ID :(' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Remove assignment from a thought
+  removeAssignment(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res
+              .status(404)
+              .json({ message: 'No thought found with that ID :(' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
